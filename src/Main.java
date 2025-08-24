dd812286b363fd458edfc50133ed6cfc6d1cfd0f
Retr0 <megaoleg5884@gmail.com>
2025-08-24 22:00:00 +0300

    Add Task 6

--- src/Main.java
+++ src/Main.java
@@ -57,5 +57,17 @@ public class Main {
 
         frog = frog +4;
         System.out.println(frog);
+
+        System.out.println();
+
+        //Task 6
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+
+        var totalWeight = boxer1 + boxer2;
+        var weightDifference = boxer2 - boxer1;
+
+        System.out.println("Общая масса двух бойцов: " + totalWeight +"кг");
+        System.out.println("Разница между массами бойцов: " + weightDifference +"кг");
     }
 }
