ec3baacbfc3230e1a09df15f750e2d3e16205525
Retr0 <megaoleg5884@gmail.com>
2025-08-24 21:52:36 +0300

    Add Task 5

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,29 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
+
         System.out.println("dog = "+ dog);
         System.out.println("cat = "+ cat);
         System.out.println("paper = "+ paper);
+
         System.out.println();
+
         //Task 2
         var Dog = dog + 4;
         var Cat = cat + 4;
         var Paper = paper + 4;
+
         System.out.println("dog + 4 = "+ Dog);
         System.out.println("cat + 4 = "+ Cat);
         System.out.println("paper + 4 = "+ Paper);
+
         System.out.println();
+
         //Task 3
         var DogMinus = dog - 3.5;
         var CatMinus = cat - 1.6;
         var PaperMinus = paper - 7639;
+
         System.out.println("dog - 3.5 = "+ DogMinus);
         System.out.println("cat - 1.6 = "+ CatMinus);
         System.out.println("paper - 7639 = "+ PaperMinus);
@@ -30,10 +37,25 @@ public class Main {
         //Task 4
         var friend = 19;
         System.out.println(friend);
+
         friend = friend + 2;
         System.out.println(friend);
+
         friend = friend / 7;
         System.out.println(friend);
+
         System.out.println();
+        //Task 5
+        var frog = 3.5;
+        System.out.println(frog);
+
+        frog = frog *10;
+        System.out.println(frog);
+
+        frog = frog / 3.5;
+        System.out.println(frog);
+
+        frog = frog +4;
+        System.out.println(frog);
     }
 }
