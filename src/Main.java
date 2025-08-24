6ef88cb98a1f270a889fb062d26db083569c132b
Retr0 <megaoleg5884@gmail.com>
2025-08-24 21:09:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog = "+ dog);
+        System.out.println("cat = "+ cat);
+        System.out.println("paper = "+ paper);
+    }
+}
