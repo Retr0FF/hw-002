96697d26ba1ea77a5c33acb4bff61bee14cceee3
Retr0 <megaoleg5884@gmail.com>
2025-08-24 22:05:03 +0300

    Add Task 7

--- src/Main.java
+++ src/Main.java
@@ -71,6 +71,7 @@ public class Main {
         System.out.println("Разница между массами бойцов: " + weightDifference +"кг");
 
         System.out.println();
+
         // Task 7
         var remaider = boxer2 % boxer1;
         System.out.println("Остаток от деления между двумя весами: " + remaider);
