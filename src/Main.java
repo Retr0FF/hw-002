803383285f77e100d88505ed8d405f5594167a82
Retr0 <megaoleg5884@gmail.com>
2025-08-24 21:18:37 +0300

    Add Task 2

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,20 @@
 public class Main {
     public static void main(String[] args)
     {
+        //Task 1
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println("dog = "+ dog);
         System.out.println("cat = "+ cat);
         System.out.println("paper = "+ paper);
+        System.out.println();
+        //Task 2
+        var Dog = dog + 4;
+        var Cat = cat + 4;
+        var Paper = paper + 4;
+        System.out.println("dog + 4 = "+ Dog);
+        System.out.println("cat + 4 = "+ Cat);
+        System.out.println("paper + 4 = "+ Paper);
     }
 }
